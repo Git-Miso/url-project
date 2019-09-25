08e7623616ae3b85ed6a1d593c02c036ed97dee4
LCLP <lukas@lclpnet.work>
2019-09-25 20:24:51 +0200

    grundlage

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function protokollText()
+{
+  document.getElementById('text').innerHTML = "Protokoll";
+}
+function domainText()
+{
+  document.getElementById('text').innerHTML = "Domain";
+}
+function pfadText()
+{
+  document.getElementById('text').innerHTML = "Pfad";
+}
+function queryText()
+{
+  document.getElementById('text').innerHTML = "QueryString";
+}
+function fragmentText()
+{
+  document.getElementById('text').innerHTML = "Fragment";
+}
