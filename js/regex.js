faa0bb78b4b6fd49a9c7999b33287b854c6cbecf
Git-Miso <vs67@protonmail.com>
2019-10-08 19:44:39 +0200

    - Inhaltmenü...

--- js/regex.js
+++ js/regex.js
@@ -1,3 +1,30 @@
+const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+const regex = new RegExp(expression);
+
 function check(textarea) {
-  
+  let t = document.getElementById("urlArea").value;
+  let display = document.getElementById("validatorStatus");
+
+  if(t === undefined) return;
+
+  let urls = t.match(regex);
+  if(urls == null || urls.length <= 0) {
+    display.innerHTML = "<i>Keine URL erkannt.</i>";
+  } else if(urls.length == 1) {
+    display.innerHTML = "Erkannte URL:<br>" + urls[0];
+  } else {
+    let html = "Erkannte URLs:<br>";
+    let i = 0;
+
+    for (url of urls) {
+      let s = "<i>" + url + "</i>"
+
+      if(i == 0) html += s;
+      else html += "<br>" + s;
+
+      i++;
+    }
+
+    display.innerHTML = html;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-function protokollText()
-{
-  document.getElementById('text').innerHTML = "Protokoll ";
-}
-function domainText()
-{
-  document.getElementById('text').innerHTML = "Domain ist geil und hat style";
-}
-function portText()
-{
-  document.getElementById('text').innerHTML = "Domain ist geil und hat style";
-}
-function pfadText()
-{
-  document.getElementById('text').innerHTML = "Pfad";
-}
-function queryText()
-{
-  document.getElementById('text').innerHTML = "QueryString";
-}
-function fragmentText()
-{
-  document.getElementById('text').innerHTML = "Das Fragment weist auf eine bestimmte Stelle im Text hin. \n Die Seite springt dann an die Stelle mit dem Objekt des Namens oder der id das dem Fragment enspricht. Also id=ziel entspricht Fragment #ziel. \n Eingesetzt wird das Fragment zum Beispiel bei Sprungstellen.";
-}
+const protokollText = "Protokoll ...";
+const subdomainText = "<b>1. Sub-Domain (Third-Level-Domain)</b><br><br> Bei Subdomains handelt es sich um Domains, die hierarchisch unter einer anderen Domain stehen. Zu erkennen sind Subdomains daran, dass sie abgetrennt von einem Punkt vor dem eigentlichen Domainnamen steht. Jede Domain kann eine oder mehrere Subdomains haben. Durch Subdomains lassen sich Inhalte unter einer gemeinsamen Adresse abrufen, aber voneinander getrennt ansprechen. ";
+const domainText = "<b>2. Domain (Second-Level-Domain)</b><br><br> Eine Domain dient einer Website als eine Internetadresse. Die IP-Adresse besteht aus verschiedenen Zahlen und Punkten wie beispielsweise 46.4.199.140. Diese lassen sich durch das Domain Name System (DNS) in Wörtern ausdrücken.";
+const topdomainText = "<b>3. Top-Level-Domain (TLD)</b><br><br> Die TLD befasst sich mit der geographischen Zuordnung oder den kommerziellen oder nicht kommerziellen Absichten einer Organisation. Diese steht hinter der Domain und ist mit einem Punkt von ihr abgetrennt.";
+const portText = "Domain ist geil und hat style";
+const pfadText ="Domain ist geil und hat style";
+const queryText = "Domain ist geil und hat style";
+const fragmentText = "<b>Das Fragment</b><br><br> Das Fragment weist auf eine bestimmte Stelle im Text hin. Die Seite springt dann an die Stelle mit dem Objekt des Namens oder der id das dem Fragment enspricht. Also id=ziel entspricht Fragment #ziel. \n Eingesetzt wird das Fragment zum Beispiel bei Sprungstellen.";
 
+
+function changeText(text) {
+  document.getElementById('text').innerHTML = text;
+}
 
 var count = 1;
 document.getElementById("myBtn").onclick = function() {
