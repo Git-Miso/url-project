8a31259701b93cdaaecaa6935a66f37dd8729cc1
Git-Miso <vs67@protonmail.com>
2019-10-09 16:07:36 +0200

    - Text überarbeitet

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const protokollText = "Protokoll ...";
+const protokollText = "<b>Das Protokoll</b><br><br>";
 const subdomainText = "<b>1. Sub-Domain (Third-Level-Domain)</b><br><br> Bei Subdomains handelt es sich um Domains, die hierarchisch unter einer anderen Domain stehen. Zu erkennen sind Subdomains daran, dass sie abgetrennt von einem Punkt vor dem eigentlichen Domainnamen steht. Jede Domain kann eine oder mehrere Subdomains haben. Durch Subdomains lassen sich Inhalte unter einer gemeinsamen Adresse abrufen, aber voneinander getrennt ansprechen. ";
-const domainText = "<b>2. Domain (Second-Level-Domain)</b><br><br> Eine Domain dient einer Website als eine Internetadresse. Die IP-Adresse besteht aus verschiedenen Zahlen und Punkten wie beispielsweise 46.4.199.140. Diese lassen sich durch das Domain Name System (DNS) in Wörtern ausdrücken.";
-const topdomainText = "<b>3. Top-Level-Domain (TLD)</b><br><br> Die TLD befasst sich mit der geographischen Zuordnung oder den kommerziellen oder nicht kommerziellen Absichten einer Organisation. Diese steht hinter der Domain und ist mit einem Punkt von ihr abgetrennt.";
-const portText = "Domain ist geil und hat style";
-const pfadText = "Domain ist geil und hat style";
-const queryText = "Domain ist geil und hat style";
-const fragmentText = "<b>Das Fragment</b><br><br> Das Fragment weist auf eine bestimmte Stelle im Text hin. Die Seite springt dann an die Stelle mit dem Objekt des Namens oder der id das dem Fragment enspricht. Also id=ziel entspricht Fragment #ziel. \n Eingesetzt wird das Fragment zum Beispiel bei Sprungstellen.";
+const domainText = "<b>2. Domain (Second-Level-Domain)</b><br><br> Eine Domain dient einer Website als eine Internetadresse. Die IP-Adresse besteht aus verschiedenen Zahlen und Punkten wie beispielsweise 46.4.199.140. Diese lassen sich durch das Domain Name System (DNS) in Wörtern ausdrücken. Dadurch können wir einfach google.de eingeben anstatt eine Zahlenkette, welche schwer zu merken ist.";
+const topdomainText = "<b>3. Top-Level-Domain (TLD)</b><br><br> Die TLD befasst sich mit der geographischen Zuordnung oder den kommerziellen oder nicht kommerziellen Absichten einer Organisation. Diese steht hinter der Domain und ist mit einem Punkt von ihr abgetrennt. <br> Beispiele sind: .org, .com, .net oder .de";
+const portText = "<b>Der Port</b><br><br> Domain ist geil und hat style";
+const pfadText = "<b>Der Pfad</b><br><br> Der Pfad gibt an wo sich die HTML-Datei auf dem Server befindet. <br> Dieses Dateisystem funktioniert wie die Dateien in einem Betriebssystem, so zum Beispiel Windows. <br> Wenn du einen Ordner öffnest wirst du auch oben so einen Pfad sehen. <br> Mit Hilfe des Pfades können verschiedene HTML-Dateien geöffnet werden, wodurch man nicht auf nur eine Seite beschränkt ist. <br> Meistens landet man wenn man eine Seite öffnet auf der Index Datei. <br> Also: www.example.de/index.html";
+const queryText = "<b>Der QueryString</b><br><br>Domain ist geil und hat style";
+const fragmentText = "<b>Das Fragment</b><br><br> Das Fragment weist auf eine bestimmte Stelle auf der Seite hin. Die Seite springt dann an die Stelle mit dem Objekt, dessen Namens oder dessen id dem Fragment enspricht. Also id=ziel entspricht Fragment #ziel. \n Eingesetzt wird das Fragment zum Beispiel bei <a href='#validatorStatus'>Sprungstellen.</a>";
 
 
 function changeText(text) {
